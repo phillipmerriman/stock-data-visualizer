bd928c3438da1542c262ce1fc2bb3be942b99556
Phillip Merriman <phillipmerriman@gmail.com>
2024-08-01 11:04:49 -0500

    set up env variables

--- src/app/services/stock-data.service.ts
+++ src/app/services/stock-data.service.ts
@@ -2,16 +2,18 @@ import { Injectable } from '@angular/core';
 import { ApiService } from './api.service';
 import { Observable } from 'rxjs';
 import { GetTickersResponse, Stock, StockParams } from '../../types';
+import { environment } from '../../environments/environment';
 
 // TODO: what is @Injectable
 @Injectable({
   providedIn: 'root',
 })
 export class StockDataService {
+  private apiKey: string = environment.apiKey;
   constructor(private apiService: ApiService) {}
 
   getTickers = (): Observable<GetTickersResponse> => {
-    const url: string = `https://api.polygon.io/v3/reference/tickers?active=true&limit=100&apiKey=0jAqt4lLvD8yXEKlGNhPLtA8UVdp6Mrg`;
+    const url: string = `https://api.polygon.io/v3/reference/tickers?active=true&limit=100&apiKey=${this.apiKey}`;
 
     return this.apiService.get(url, {
       responseType: 'json',
@@ -21,7 +23,7 @@ export class StockDataService {
   //TODO: what is Observable
   // Observables are lazy push collections of multiple values: https://rxjs.dev/guide/observable
   getStockData = (params: StockParams): Observable<Stock> => {
-    const url: string = `https://api.polygon.io/v2/aggs/ticker/${params.ticker}/range/${params.multiplier}/${params.timespan}/${params.from}/${params.to}?apiKey=0jAqt4lLvD8yXEKlGNhPLtA8UVdp6Mrg`;
+    const url: string = `https://api.polygon.io/v2/aggs/ticker/${params.ticker}/range/${params.multiplier}/${params.timespan}/${params.from}/${params.to}?apiKey=${this.apiKey}`;
 
     return this.apiService.get(url, {
       params,
